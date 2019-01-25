6cbc9e7899d4f70c8049c2504a196485235eeee7
Faysal <faysal024012@gmail.com>
2019-01-25 18:17:21 +0600

    admin settings panel added

--- abdat-plugin.php
+++ abdat-plugin.php
@@ -1,4 +1,39 @@
 <?php
+/**
+ * @package  AbdatPlugin
+ */
 /*
-* PluginName
+Plugin Name: Abdat Plugin
+Plugin URI: https://github.com/faysalduet/abdat-plugin
+Description: This is my first attempt on writing a custom Plugin .
+Version: 1.0.0
+Author: Faysal 
+Author URI:https://github.com/faysalduet/
+License: Not define
+Text Domain: abdat-plugin
 */
+
+defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
+
+// Require once the Composer Autoload
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+
+function activate_abdat_plugin() {
+	Inc\Base\Activate::activate();
+}
+register_activation_hook( __FILE__, 'activate_abdat_plugin' );
+
+
+
+function deactivation_abdat_plugin(){
+	Inc\Base\Deactivate::deactivate();
+}
+register_deactivation_hook(__FILE__, 'deactivate_abdat_plugin');
+
+
+if ( class_exists( 'Inc\\Init' ) ) {	
+	Inc\Init::registerServices();
+}
---
+++ templates/admin.php
@@ -0,0 +1,25 @@
+<div class="wrap">
+	<h1>Abdat Plugin</h1>
+	<?php settings_errors(); ?>
+
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#tab-1">Manage Settings</a></li>
+	
+	</ul>
+
+	<div class="tab-content">
+		<div id="tab-1" class="tab-pane active">
+
+			<form method="post" action="options.php">
+				<?php 
+					settings_fields( 'abdat_plugin_settings' );
+					do_settings_sections( 'abdat_plugin' );
+					submit_button();
+				?>
+			</form>
+			
+		</div>
+
+		
+	</div>
+</div>
